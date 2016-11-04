049dae9354ac21fbb2c3580551aa614ca11a66f1
Robert <rlancer@arachno.com>
2016-11-04 02:33:59 -0400

    listen for debates to happen

--- src/logic/system.js
+++ src/logic/system.js
@@ -5,8 +5,8 @@ import Peer from 'simple-peer';
 let userId = localStorage.getItem("user");
 
 const updatePing = ()=>firebase.database().ref(`yooo/${userId}`).set({time: (new Date()).getTime()});
-window.setInterval(updatePing, 15000);
-updatePing();
+window.setInterval(updatePing, 30000);
+
 
 if (!userId) {
   userId = shortid.generate();
@@ -23,41 +23,17 @@ const getMingler = async()=> {
 
 const hookMeUpRef = firebase.database().ref(`hookmeup/${userId}`);
 
-
-// order by the date they joined
-// make a request to the one cient above you and bellow you
-
-
-// to avoid conflicts make a client a pitcher or a catcher
-
-// pitchers send requests
-// catchers receive requests
+hookMeUpRef.on('child_added', (data) => {
+  console.log('HOOK ME UP!!!!!', data.key, data.val());
 
 
-// have a date joined timestap for when there session starts
+  // see if it should be initi or not
+  /*const myPeer = new Peer({initiator: true, trickle: false, stream});
 
-// get random client to debate with
-// create a debate room record under that client with said signaling info
-
-
-// client listens of debate rooms and connects if it has one
-
-// when client connects to a room it sets the status form pending to  connecting
-// it then takes all the other records under its room and it deletes them
-
-// when client that sent out the debate notifications gets the signal that its connecting, it should delete
-
-
-// notify client that it wants to debate
-
-// listen for clients trying to debate
-// if it gets a client its trying to debate with send an ak
-
-// listen for ak and start up a debate
-
-
-// client with greater ID will be akknowdleger
-
+   myPeer.on('signal', (data)=> {
+   firebase.database().ref(`mystream/${userId}`).set(data);
+   });*/
+});
 
 let stream = false;
 
@@ -79,16 +55,16 @@ export default {
       firebase.database().ref(`available/${userId}`).set(toSet);
     });
 
-    peerSelf.on('error', function (err) {
+    peerSelf.on('error', (err)=> {
       console.log('error', err);
     });
 
-    peerSelf.on('connect', function () {
+    peerSelf.on('connect', () => {
       console.log('CONNECT');
       peerSelf.send('whatever' + Math.random());
     });
 
-    peerSelf.on('data', function (data) {
+    peerSelf.on('data', (data)=> {
       console.log('data: ' + data)
     });
 
@@ -102,13 +78,10 @@ export default {
 
     peerSelf.signal(JSON.parse(clientSignal));
 
-
     peerSelf.on('connect', () => {
       console.log('peer self connected!');
       // wait for 'connect' event before using the data channel
       peerSelf.send('hey peer2, how is it going?');
     });
-
-
   }
 }
