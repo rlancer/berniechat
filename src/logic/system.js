c0528aab98aa6b451ab00c551c50de05ae3e4092
Robert <rlancer@arachno.com>
2016-11-16 22:19:18 -0500

    using xirsys server

--- src/logic/system.js
+++ src/logic/system.js
@@ -1,32 +1,43 @@
 import firebase from './firebase';
 import SimplePeer from 'simple-peer';
+import request from 'superagent';
+
+const getConfig = async()=>
+  (await request.get('https://service.xirsys.com/ice').query({
+    ident: "rlancer",
+    secret: "c2f49b54-aaed-11e6-bd39-178cacb04d70",
+    domain: "debateoff.live",
+    application: "default",
+    room: "default",
+    secure: 1
+  })).d;
 
 export default {
-  start(view){
-
+  async start(view){
+    const iceConfig = await getConfig();
     navigator.getUserMedia({audio: true, video: false},
       stream=> {
         console.log('stream is user', stream);
         view.setupStream(stream, true);
-
+        
         let user = false;
-
+        
         const initiate = ()=> {
           var isAnonymous = user.isAnonymous;
           var uid = user.uid;
-
-          var peer1 = new SimplePeer({initiator: true, stream, trickle: true});
-
+          
+          var peer1 = new SimplePeer({initiator: true, stream, trickle: true, config: iceConfig});
+          
           firebase.database().ref(`join/${uid}`).on('value', snap=> {
             const v = snap.val();
-
+            
             if (!v)
               return;
-
+            
             console.log('got value from other peer', v);
-
+            
             const timeAgo = ((new Date()).getTime() - v.time) / 1000;
-
+            
             if (timeAgo > 4) {
               console.log('too long ago ignore');
             }
@@ -35,68 +46,68 @@ export default {
               v.signals.forEach(signal=>peer1.signal(signal));
             }
           });
-
+          
           const signals = [];
           peer1.on('signal', data => {
             signals.push(data);
-
+            
             if (signals.length === 2)
               firebase.database().ref('init/' + uid).set(signals);
           });
-
+          
           peer1.on('connect', ()=> {
             console.log('hey peer2, how is it going?');
             peer1.send('hey peer2, how is it going?');
           });
-
+          
           peer1.on('stream', (stream) => {
             console.log('got stream', stream);
             view.setupStream(stream, false);
           });
-
-
+          
+          
           peer1.on('data', (data) => console.log('got a message from peer2: ' + data));
         };
-
+        
         const joinClient = async token=> {
           const signal = await firebase.database().ref('init/' + token).once('value');
           console.log(signal.val(), signal.key);
-
-
-          var peer2 = new SimplePeer({initiator: false, stream, trickle: true});
-
+          
+          
+          var peer2 = new SimplePeer({initiator: false, stream, trickle: true, config: iceConfig});
+          
           signal.val().forEach(signal=>peer2.signal(signal));
-
+          
           const signals = [];
           peer2.on('signal', (data) => {
             signals.push(data);
-
+            
             if (signals.length === 2)
               firebase.database().ref('join/' + token).set({time: (new Date()).getTime(), signals});
           });
-
+          
           peer2.on('connect', ()=> {
             // wait for 'connect' event before using the data channel
             console.log('Per2 send hey per1 how go it?');
             peer2.send('hey peer1, how is it going?')
           });
-
+          
           peer2.on('stream', (stream) => {
             console.log('got stream', stream);
             view.setupStream(stream, false);
           });
-
+          
           peer2.on('data', (data) => console.log('got a message from peer1: ' + data));
         };
-
+        
         firebase.auth().onAuthStateChanged(u => {
           if (u) {
             user = u;
-
+            
             const path = window.location.pathname.substring(1);
-
+            
             const join = path.startsWith('join');
-
+            
             if (!join) {
               view.setJoinURL(`${window.location.origin}/join/${user.uid}`);
               initiate();
@@ -108,8 +119,8 @@ export default {
             console.log('user signed out ..........');
           }
         });
-
-
+        
+        
         window.setTimeout(()=> {
           console.log('AUTH CCC');
           if (!user) {
@@ -118,12 +129,12 @@ export default {
               // Handle Errors here.
               var errorCode = error.code;
               var errorMessage = error.message;
-
+              
               console.error(error);
             });
           }
         }, 4000);
-
+        
       }, err=>console.error('Could not get stream', err));
   }
 }
