88fd5c7a94e6ebc45f94a50f9e600856f7ce6058
Robert <rlancer@arachno.com>
2016-11-22 02:17:59 -0500

    workng with twilio video

--- src/App.js
+++ src/App.js
@@ -2,33 +2,35 @@ import React, {Component} from 'react';
 import './App.css';
 import system from './logic/twil';
 import Puppet from './components/Puppet';
-import Peer from 'peerjs';
+// import Peer from 'peerjs';
 
 class App extends Component {
-
+  
   constructor(props) {
     super(props);
     this.state = {joinUrl: false, hasSelf: false, hasOther: false};
   }
-
+  
   componentDidMount() {
     system.start(this);
   }
-
+  
   puppets = [];
-
+  
+  
   setupStream = (stream, isSelf)=> {
     console.log('setup stream in app', isSelf);
     this.puppets.push(<Puppet key={isSelf ? 'self' : 'other'} stream={stream} isSelf={isSelf}/>);
     this.setState({[isSelf ? 'hasSelf' : 'hasOther']: true});
   };
-
+  
+  
   setJoinURL = joinUrl=>
     this.setState({joinUrl});
-
+  
   render() {
     const {joinUrl, hasSelf, hasOther} = this.state;
-
+    
     return (
       <div className="App">
         {joinUrl}
--- src/logic/twil.js
+++ src/logic/twil.js
@@ -8,28 +8,62 @@ const getConfig = async()=>
 const getIdent = async()=>
   (await request.get('https://debateoff-back-derdepupkp.now.sh/vid')).body;
 
+// eslint-disable-next-line
+const twilio = Twilio;
+
+const getUserMedia = ()=>new Promise((resolve, reject)=>
+  navigator.getUserMedia({audio: true, video: false}, stream=>resolve(stream), err=>reject(err))
+);
+
+const set = new Set();
+set.add('a');
+set.add('b');
+set.add('c');
+console.log(set);
+console.log(set.values());
 
 export default {
-  
   
   async start(view){
     
     const {identity, token} = await getIdent();
     
-    // eslint-disable-next-line
-    const client = new Twilio.Video.Client(token);
+    const client = new twilio.Video.Client(token);
     
     client.connect({to: 'room-name'}).then(room => {
       console.log('Connected to Room "%s"', room.name);
       
+      const l = participant=> {
+        console.log('PARTIICPANT');
+        // console.log('participant.media.mediaStreams',  participant.media.tracks);
+  
+        participant.media.on('trackAdded', function trackAdded(track) {
+          console.log('Track added', track.mediaStream, participant);
+          view.setupStream(track.mediaStream);
+        });
+        
+        participant.media.tracks.forEach(track=> {
+          console.log({track});
+        });
+        // console.log('participant.media.mediaStreams.values()', JSON.stringify(participant.media.mediaStreams.values()));
+        /*console.log('participant.media.mediaStreams.entries()', participant.media.mediaStreams.entries());
+         console.log('participant.media.mediaStreams', participant.media.mediaStreams);
+         console.log('participant.media.mediaStreams.entries().next().value', participant.media.mediaStreams.entries().next().value);*/
+      };
+      
       room.participants.forEach(participant => {
-        console.log('Participant "%s" is connected', participant.identity,participant);
+        // console.log('Participant "%s" is connected', participant.identity, participant, participant.media.audioTracks);
         // view.setupStream(participant.media);
-        participant.media.attach(document.body);
+        //   participant.media.attach(document.body);
+        l(participant);
+        // debugger;
       });
       
       room.on('participantConnected', participant => {
-        console.log('Participant "%s" connected', participant.identity);
+        // console.log('Participant "%s" connected', participant.identity);
+        // participant.media.attach(document.body);
+        
+        l(participant);
         // participant.media.attach(document.body);
       });
       
@@ -37,6 +71,5 @@ export default {
         console.log('Participant "%s" disconnected', participant.identity);
       });
     });
-    
   }
 }
