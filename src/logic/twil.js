9062fabb2788fc38fc1b21fe6b6d9b7c2b59cabf
Robert <rlancer@arachno.com>
2016-11-22 22:15:45 -0500

    audio only

--- src/logic/twil.js
+++ src/logic/twil.js
@@ -1,5 +1,4 @@
 import firebase from './firebase';
-import SimplePeer from 'simple-peer';
 import request from 'superagent';
 
 const getConfig = async()=>
@@ -21,50 +20,50 @@ export default {
     
     const {identity, token} = await getIdent();
     
-    console.log('IDENTITY / TOKEN', identity, token);
     const client = new twilio.Video.Client(token);
     
-    client.connect({to: 'room-name'}).then(room => {
-      console.log('Connected to Room "%s"', room.name);
+    const localMedia = new twilio.Video.LocalMedia();
+    await localMedia.addMicrophone();
+    
+    const room = await client.connect({to: 'room-name', localMedia});
+    console.log('Connected to Room "%s"', room.name);
+    
+    const l = participant=> {
+      console.log('PARTIICPANT', participant);
+      // console.log('participant.media.mediaStreams',  participant.media.tracks);
       
-      const l = participant=> {
-        console.log('PARTIICPANT', participant);
-        // console.log('participant.media.mediaStreams',  participant.media.tracks);
-        
-        participant.media.on('trackAdded', function trackAdded(track) {
-          view.setupStream({stream:track.mediaStream, identity:participant.identity});
-        });
-        
-        
-        /* might need to use this? look into it if track added fails
-         participant.media.tracks.forEach(track=> {
-         
-         });*/
-        // console.log('participant.media.mediaStreams.values()', JSON.stringify(participant.media.mediaStreams.values()));
-        /*console.log('participant.media.mediaStreams.entries()', participant.media.mediaStreams.entries());
-         console.log('participant.media.mediaStreams', participant.media.mediaStreams);
-         console.log('participant.media.mediaStreams.entries().next().value', participant.media.mediaStreams.entries().next().value);*/
-      };
-      
-      room.participants.forEach(participant => {
-        // console.log('Participant "%s" is connected', participant.identity, participant, participant.media.audioTracks);
-        // view.setupStream(participant.media);
-        //   participant.media.attach(document.body);
-        l(participant);
-        // debugger;
+      participant.media.on('trackAdded', function trackAdded(track) {
+        view.setupStream({stream: track.mediaStream, identity: participant.identity});
       });
       
-      room.on('participantConnected', participant => {
-        // console.log('Participant "%s" connected', participant.identity);
-        // participant.media.attach(document.body);
-        
-        l(participant);
-        // participant.media.attach(document.body);
-      });
+      /* might need to use this? look into it if track added fails
+       participant.media.tracks.forEach(track=> {
+       
+       });*/
+      // console.log('participant.media.mediaStreams.values()', JSON.stringify(participant.media.mediaStreams.values()));
+      /*console.log('participant.media.mediaStreams.entries()', participant.media.mediaStreams.entries());
+       console.log('participant.media.mediaStreams', participant.media.mediaStreams);
+       console.log('participant.media.mediaStreams.entries().next().value', participant.media.mediaStreams.entries().next().value);*/
+    };
+    
+    room.participants.forEach(participant => {
+      // console.log('Participant "%s" is connected', participant.identity, participant, participant.media.audioTracks);
+      // view.setupStream(participant.media);
+      //   participant.media.attach(document.body);
+      l(participant);
+      // debugger;
+    });
+    
+    room.on('participantConnected', participant => {
+      // console.log('Participant "%s" connected', participant.identity);
+      // participant.media.attach(document.body);
       
-      room.on('participantDisconnected', participant => {
-        console.log('Participant "%s" disconnected', participant.identity);
-      });
+      l(participant);
+      // participant.media.attach(document.body);
+    });
+    
+    room.on('participantDisconnected', participant => {
+      console.log('Participant "%s" disconnected', participant.identity);
     });
   }
 }
