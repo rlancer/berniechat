bb87fb0b1c47bf35d5fc99bb761a561a7df9edd9
Robert <rlancer@arachno.com>
2016-12-02 02:18:58 -0500

    charecters attaches to logic

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,13 +1,21 @@
-import React, {Component} from 'react';
-const characters = [{key: 'larryasbernie', path: 'LarryAsBernie.png', splitPoint: 10}];
+import React from 'react';
+import Component from './Component'
 
-class Characters extends Component {
+
+export default class Characters extends Component {
+  
+  constructor() {
+    super();
+    this.characters = [{key: 'larryasbernie', path: 'LarryAsBernie.png', splitPoint: 10}];
+  }
+  
+  componentDidMount() {
+    this.logic.characters = this;
+  }
   
   render() {
     return <div style={{border: 'solid 1px #eee'}}>
-      {characters.map(char => <img src={`/characters/${char.path}`} key={char.key} ref={c => char.ref = c}/>)}
+      {this.characters.map(char => <img src={`/characters/${char.path}`} key={char.key} ref={c => char.ref = c}/>)}
     </div>
   }
 }
-
-export default Characters;
