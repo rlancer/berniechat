f0000134242eebb0b9781cb2f61982188aeccdeb
Robert Lancer <robert.lancer@gmail.com>
2017-03-11 20:38:44 -0500

    using twilo from package

--- src/logic/Twil.js
+++ src/logic/Twil.js
@@ -1,6 +1,6 @@
-import firebase from './firebase';
 import request from 'superagent';
 import shortId from 'shortid';
+import Video from 'twilio-video';
 
 const getIdent = async(character) => {
   const now = 'https://debateoff-back-qfkezwpxqg.now.sh';
@@ -9,7 +9,6 @@ const getIdent = async(character) => {
 }
 
 // eslint-disable-next-line
-const twilio = Twilio;
 
 const getUserMedia = () => new Promise((resolve, reject) =>
   navigator.getUserMedia({audio: true, video: false}, stream => resolve(stream), err => reject(err))
@@ -18,32 +17,32 @@ const getUserMedia = () => new Promise((resolve, reject) =>
 let started = false;
 
 export default class Twil {
-  
+
   constructor({logic}) {
     this.logic = logic;
   }
-  
+
   async init(character) {
     console.log('init with char', character);
     const {identity, token} = await getIdent(character);
     console.log('identity', identity);
     this.logic.app.setIdentity(identity);
     this.identity = identity;
-    this.client = new twilio.Video.Client(token);
-    this.localMedia = new twilio.Video.LocalMedia();
+    this.client = new Video.Client(token);
+    this.localMedia = new Video.LocalMedia();
     this.mic = await this.localMedia.addMicrophone();
-    
+
     await this.start();
   }
-  
+
   async start() {
-    
+
     this.roomId = false;
     const path = window.location.pathname.substring(1);
-    
+
     const parts = path.split('/');
     this.roomId = parts[0].trim();
-    
+
     if (this.roomId.length > 0 && this.roomId !== 'undefined')
       this.logic.app.setJoinedRoom({room: this.roomId});
     else {
@@ -51,19 +50,19 @@ export default class Twil {
       this.logic.app.setJoinURL({room: this.roomId});
       this.pushRoomToPath();
     }
-    
+
     const room = await this.client.connect({to: this.roomId, localMedia: this.localMedia});
     this.logic.add({stream: this.mic.mediaStream, identity: room.localParticipant.identity, isSelf: true});
-    
+
     room.on('trackAdded', (track, participant) =>
       this.logic.add({stream: track.mediaStream, identity: participant.identity, isSelf: this.identity === participant.identity}));
-    
+
     room.on('participantDisconnected', (participant) =>
       this.logic.remove({identity: participant.identity}));
-    
+
     window.addEventListener('unload', () => room.disconnect());
   }
-  
+
   pushRoomToPath() {
     //   history.pushState({room: this.roomId}, "Chatting!", `/${this.roomId}`);
   }
