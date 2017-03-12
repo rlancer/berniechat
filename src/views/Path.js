ab8cd7b5a549b845cd96ed53cf811afffb85bc8b
Robert Lancer <robert.lancer@gmail.com>
2017-03-11 22:27:56 -0500

    branding

--- src/views/Path.js
+++ src/views/Path.js
@@ -4,25 +4,25 @@ import copy from 'copy-to-clipboard';
 import Snackbar from 'material-ui/Snackbar';
 
 export default class Path extends Component {
-  
+
   constructor(props) {
     super(props);
     this.state = {showedCopied: false};
   }
-  
+
   copyUrl = () => {
     copy(this.url);
     this.setState({showedCopied: true});
   };
-  
+
   get url() {
     const {roomId} = this.logic.twil;
-    return `${this.isLocal ? 'http://localhost:3000' : 'https://bernie.chat'}/${roomId}`;
+    return `${this.isLocal ? 'http://localhost:3000' : 'https://trumpaphone.com'}/${roomId}`;
   }
-  
+
   closeShowCopied = () =>
     this.setState({showedCopied: false});
-  
+
   render() {
     const {showedCopied} = this.state;
     return <div style={{padding: '1.5rem', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
@@ -31,7 +31,7 @@ export default class Path extends Component {
       </div>
       <Snackbar autoHideDuration={2500} message='Copied to clipboard' open={showedCopied} onRequestClose={this.closeShowCopied}/>
     </div>;
-    
+
   }
 }
 
